d971d0b5f83c1e78f6c3fd86f00792d9fabe1a20
Andrew <andrewsh@gmail.com>
2012-05-26 12:57:29 -1000

    Added .gitignore and .config (cairo-dock)

--- makesymlinks.sh
+++ makesymlinks.sh
@@ -8,7 +8,7 @@
 
 dir=~/.dotfiles                    # dotfiles directory
 olddir=~/.dotfiles_old             # old dotfiles backup directory
-files=".fluxbox .profile .tilda .zshrc"    # list of files/folders to symlink in homedir
+files=".config .fluxbox .profile .tilda .zshrc"    # list of files/folders to symlink in homedir
 
 ##########
 
