41198d16c83afed561787a07b62da060d067ad1d
Andrew <andrewsh@gmail.com>
2012-07-24 15:36:33 -1000

    new file:   .wallpaper/InfinitesimalBlue.jpg 	new file:   .wallpaper/World_Panoramas_Scenery_2560x1024_pan0037h622.jpg 	new file:   .wallpaper/World_Panoramas_Scenery_2560x1024_pan0038h634.jpg 	new file:   .wallpaper/[AnimePaper]wallpapers_Toradora!-_Xyrick(1.6)_1920x1200_106766.jpg 	new file:   .wallpaper/[wallcoo.com]_2880x900_DualScreen_Nature_Wallpaper_031208.jpg 	new file:   .wallpaper/[wallcoo.com]_2880x900_DualScreen_Nature_Wallpaper_071612.jpg 	new file:   .wallpaper/[wallcoo.com]_2880x900_DualScreen_Nature_Wallpaper_081713.jpg 	new file:   .wallpaper/[wallcoo.com]_2880x900_DualScreen_Nature_Wallpaper_091814.jpg 	new file:   .wallpaper/[wallcoo.com]_2880x900_DualScreen_Nature_Wallpaper_223127.jpg 	new file:   .wallpaper/adolescenteyes.jpg 	new file:   .wallpaper/gallery_2_37_51573.jpg 	new file:   .wallpaper/gallery_2_78_651080.jpg 	new file:   .wallpaper/gallery_2_84_1029191.jpg 	new file:   .wallpaper/gallery_2_84_511486.jpg 	new file:   .wallpaper/gallery_2_84_542901.jpg 	new file:   .wallpaper/koshelkov-lapd.jpg 	new file:   .wallpaper/moe 184671 k_kanehira landscape 2560x1600.jpg 	new file:   .wallpaper/moe 192104 kurono-kuro landscape 2560x1600.jpg 	new file:   .wallpaper/wallpaper_by_sandara.jpg 	new file:   .wallpaper/watchingtheheavensfall.jpg 	modified:   makesymlinks.sh

--- makesymlinks.sh
+++ makesymlinks.sh
@@ -8,7 +8,7 @@
 
 dir=~/.dotfiles                    # dotfiles directory
 olddir=~/.dotfiles_old             # old dotfiles backup directory
-files=".gitconfig .zprofile .zshrc"    # list of files/folders to symlink in homedir
+files=".gitconfig .zprofile .zshrc .wallpaper"    # list of files/folders to symlink in homedir
 
 ##########
 
